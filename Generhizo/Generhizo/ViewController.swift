3778b317798807ea7bd79228e7161252dec6f7e1
cor <cor@pruijs.nl>
2018-04-13 21:17:21 +0200

    Add empty Xcode project

---
+++ Generhizo/Generhizo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Generhizo
+//
+//  Created by Cor Pruijs on 13-04-18.
+//  Copyright © 2018 Cor Pruijs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

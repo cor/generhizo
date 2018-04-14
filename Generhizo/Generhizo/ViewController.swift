ee080c6681977e0ae19346374ec3b75bf4a33b3f
cor <cor@pruijs.nl>
2018-04-14 10:34:21 +0200

    Add basic CALayer creation

--- Generhizo/Generhizo/ViewController.swift
+++ Generhizo/Generhizo/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var generhizoButtonPressed: UIButton!
+    
+    @IBAction func generhizoButtonPressed(_ sender: UIButton) {
+        
+        
+        view.layer.backgroundColor = UIColor.green.cgColor
+        
+        for x in -5...5 {
+            for y in -5...5 {
+                view.layer.addSublayer(sublayerConstructor(x: CGFloat(x * 150), y: CGFloat(y * 150)))
+            }
+        }
+    }
+    
+    private func sublayerConstructor(x: CGFloat, y: CGFloat) -> CALayer {
+        let sublayer = CALayer()
+        sublayer.backgroundColor = UIColor.red.cgColor
+        sublayer.frame = CGRect(x: view.layer.frame.width / 2 - 50 + x, y: view.layer.frame.height / 2 - 50 + y, width: 100, height: 100)
+        return sublayer
+    }
 
 }
 
